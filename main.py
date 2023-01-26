f69948bc1fcf5d5016d2c067bb5a5277a43b9c4f
Alam <ashahjade@gmail.com>
2023-01-25 23:37:49 -0500

    commit

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
-# screen.title("My Snake Game")
-screen.tracer(0)
+screen.title("Ping Pong")
+# screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+
+is_game_on = False
+while is_game_on:
+    screen.update()
+
+
 
 
 screen.exitonclick()
