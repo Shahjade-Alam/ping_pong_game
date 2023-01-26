4186ce51bf9f095c1857b872befd16798940cf26
Alam <ashahjade@gmail.com>
2023-01-25 23:02:43 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+# screen.title("My Snake Game")
+screen.tracer(0)
+
+
+screen.exitonclick()
