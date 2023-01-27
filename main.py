866419dcdfc046b49f355bcaab34885277b1c99d
Alam <ashahjade@gmail.com>
2023-01-26 22:45:48 -0500

    commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scroeboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,8 @@ screen.title("Ping Pong")
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard=Scoreboard()
+
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -20,14 +23,30 @@ screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
-is_game_on = False
+is_game_on = True
 while is_game_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     # Detect collision with ball
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with right paddle
+    if ball.distance(r_paddle) <50 and ball.xcor()>320 or ball.distance(l_paddle)<50 and ball.xcor()<-320:
+        ball.bounce_x()
+
+    # Detect collision with right paddle
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect collision with left paddle
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
 
 screen.exitonclick()
